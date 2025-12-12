355318b822b5374417bb8da89811d2ae9487701b
Bryan <br1173626@gmail.com>
2025-12-12 11:29:54 -0400

    goes through a binary tree using pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,12 +7,12 @@
  * The value in the node must be passed as a parameter to this function.
  * Return: nothing
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))  
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
